d6bd87db5d2c3760d69910dd7dcafb8b78bc84c2
Leillen <150943989+Leillen59@users.noreply.github.com>
2025-04-22 11:10:01 +0200

    ajout header, footer et création des pages

---
+++ src/app/Pages/batiment/batiment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-batiment',
+  standalone: true,
+  imports: [],
+  templateUrl: './batiment.component.html',
+  styleUrl: './batiment.component.scss'
+})
+export class BatimentComponent {
+
+}
---
+++ src/app/Pages/fiche-artisan/fiche-artisan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fiche-artisan',
+  standalone: true,
+  imports: [],
+  templateUrl: './fiche-artisan.component.html',
+  styleUrl: './fiche-artisan.component.scss'
+})
+export class FicheArtisanComponent {
+
+}
